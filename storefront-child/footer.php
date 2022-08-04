5d9cc1fde2fcf1d7a9034ef7f287613ed256ee5d
wJumelle <wjumelle@gmail.com>
2022-08-04 19:37:41 +0200

    [HP] Intégration du header, du footer et de la hero cover

---
+++ storefront-child/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package storefront
+ */
+
+?>
+
+		</div><!-- .col-full -->
+	</div><!-- #content -->
+
+	<?php /* do_action( 'storefront_before_footer' ); */ ?>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="col-full">© Madeleine et Lucette 2022</div>
+	</footer>
+
+	<?php /* do_action( 'storefront_after_footer' ); */ ?>
+
+</div><!-- #page -->
+
+<?php /* wp_footer(); */ ?>
+
+</body>
+</html>
