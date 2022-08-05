11880f6217bc1371f5b1301e6699619af5135eb4
wJumelle <wjumelle@gmail.com>
2022-08-05 18:36:21 +0200

    [HP] Début contribution des sections Notre catalogue et Notre équipe

--- storefront-child/functions.php
+++ storefront-child/functions.php
@@ -26,23 +26,46 @@ function sf_child_theme_dequeue_style() {
 remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart');
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 20);
 
-
 /**
- * Surcharge de la fonction permettant d'afficher le panier 
- * dans le header du thème.
- * Chemin de la fonction : /wordpress/wp-content/themes/storefront/inc/woocommerce/storefront-woocommerce-template-functions.php
+ * Surcharge des fonctions du header
+ * Chemin des fonctions : /wordpress/wp-content/themes/storefront/inc/woocommerce/storefront-woocommerce-template-functions.php et 
+ * /wordpress/wp-content/themes/storefront/inc/storefront-template-functions.php
  */
-
-/**
- * Display Header Cart
- *
- * @since  1.0.0
- * @uses  storefront_is_woocommerce_activated() check if WooCommerce is activated
- * @return void
- */
-function storefront_header_cart() {
-    // Nothing to show here
+function storefront_product_search() {}
+function storefront_header_cart() {}
+function storefront_header_container() {
+    echo '<div class="col-full flex flex-row flex-align-center flex-justify-spaceBetween">';
 }
+function storefront_header_container_close() {}
+function storefront_primary_navigation_wrapper() {
+    echo '<div class="storefront-primary-navigation">';
+}
+function storefront_primary_navigation_wrapper_close() {
+    echo '</div>';
+}
+function storefront_primary_navigation() {
+    ?>
+		<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary Navigation', 'storefront' ); ?>">
+		<button id="site-navigation-menu-toggle" class="menu-toggle" aria-controls="site-navigation" aria-expanded="false"><span><?php echo esc_html( apply_filters( 'storefront_menu_toggle_text', __( 'Menu', 'storefront' ) ) ); ?></span></button>
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location'  => 'primary',
+					'container_class' => 'primary-navigation',
+				)
+			);
+			?>
+		</nav><!-- #site-navigation -->
+		<?php
+}
+
+/**
+ * Surcharge des fonctions de la homepage
+ */
+function storefront_homepage_header() {
+    edit_post_link( __( 'Edit this section', 'storefront' ), '', '', '', 'button storefront-hero__button-edit' );
+}
+
 
 /**
  * Surcharge de la fonction permettant d'afficher la sticky bar
--- storefront-child/header.php
+++ storefront-child/header.php
@@ -16,6 +16,9 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 </head>
 
 <body <?php body_class(); ?>>
