38ea4fd2b55c035ef04d9a8d2cc59a565827649f
wJumelle <wjumelle@gmail.com>
2022-08-14 11:03:34 +0200

    [SIDEBAR] Ajout des remontées du devis en cours, affichage à faire

--- storefront-child/sidebar.php
+++ storefront-child/sidebar.php
@@ -2,6 +2,7 @@
 /**
  * The sidebar containing the main widget area.
  * Le contenu de la sidebar est configurable dans le backend via Apparence > Widgets
+ * Le contenu de la sidebar a été conçu via le code du fichier /wp-content/plugins/yith-woocommerce-request-a-quote/templates/request-quote-view.php
  *
  * @package storefront
  */
@@ -12,5 +13,57 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <div id="secondary" class="widget-area mel-sidebar" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<?php 
+		// On récupère les données du plugin YITH que l'on stocke dans une variable
+		$raqSidebar = YITH_Request_Quote()->get_raq_return();
+		//var_dump($raqSidebar);
+
+		// Si le devis est vide, alors on affiche un message + potentiellement d'autre contenu ?
+		// Sinon on affiche les éléments qui compose le devis
+		if ( count( $raqSidebar ) === 0 ) { ?>
+			<p><?php esc_html_e( 'No products in list', 'yith-woocommerce-request-a-quote' ); ?></p>
+		<?php } else { 
+			foreach ( $raqSidebar as $key => $raq ) {
+				// On récupère toutes les informations autour du produit
+				$product_id = ( isset( $raq['variation_id'] ) && '' !== $raq['variation_id'] ) ? $raq['variation_id'] : $raq['product_id'];
+				$_product = wc_get_product( $product_id );
+				if ( ! isset( $_product ) || ! is_object( $_product ) ) {
+					continue;
+				}
+
+				// On récupère l'image du produit 
+				$thumbnail = $_product->get_image();
+				if ( ! $_product->is_visible() ) {
+					echo $thumbnail; //phpcs:ignore
+				} else {
+					printf( '<a href="%s">%s</a>', $_product->get_permalink(), $thumbnail ); //phpcs:ignore
+				}
+
+				// On récupère le nom du produit
+				$product_title = $_product->get_title();
+				if ( $_product->get_sku() !== '' && get_option( 'ywraq_show_sku' ) === 'yes' ) {
+					$product_title .= ' ' . apply_filters( 'ywraq_sku_label', __( ' SKU:', 'yith-woocommerce-request-a-quote' ) ) . $_product->get_sku();
+				}
+				echo $product_title;
+
+				// On récupère la quantité
+				// Si jamais on veut que la sidebar puisse mettre à jour le devis
+				// $product_quantity = woocommerce_quantity_input(
+				// 	array(
+				// 		'input_name'  => "raq[{$key}][qty]",
+				// 		'input_value' => apply_filters( 'ywraq_quantity_input_value', $raq['quantity'] ),
+				// 		'max_value'   => apply_filters( 'ywraq_quantity_max_value', $_product->backorders_allowed() ? '' : $_product->get_stock_quantity(), $_product ),
+				// 		'min_value'   => apply_filters( 'ywraq_quantity_min_value', 0, $_product ),
+				// 		'step'        => apply_filters( 'ywraq_quantity_step_value', 1, $_product ),
+				// 	),
+				// 	$_product,
+				// 	false
+				// );
+				//Sinon plus simplement on peut juste echo la quantité du produit
+				$product_quantity = $raq['quantity'];
+
+				echo $product_quantity;
+			}
+		}
+	?>
 </div><!-- #secondary -->
