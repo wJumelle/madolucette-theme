baf1965c0d180a9c500212a63b3b423c8966651d
wJumelle <wjumelle@gmail.com>
2022-11-03 22:25:32 +0100

    [UPDATE] Màj WooCommerce en v7.0.1

--- storefront-child/woocommerce/product-searchform.php
+++ storefront-child/woocommerce/product-searchform.php
@@ -7,7 +7,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 3.3.0
+ * @version 7.0.1
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -18,6 +18,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 <form role="search" method="get" class="woocommerce-product-search mel-product-search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label class="screen-reader-text" for="woocommerce-product-search-field-<?php echo isset( $index ) ? absint( $index ) : 0; ?>">Chercher un produit...</label>
 	<input type="search" id="woocommerce-product-search-field-<?php echo isset( $index ) ? absint( $index ) : 0; ?>" class="search-field" placeholder="Chercher un produit..." value="<?php echo get_search_query(); ?>" name="s" />
-	<button type="submit" value="Rechercher">Rechercher</button>
+	<button type="submit" value="Rechercher" class="<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ); ?>">Rechercher</button>
 	<input type="hidden" name="post_type" value="product" />
 </form>
