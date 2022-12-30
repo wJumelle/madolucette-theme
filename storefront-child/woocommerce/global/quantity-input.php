096aa3afb84fc688bb97799ddcd89e174defa7ec
wJumelle <wjumelle@gmail.com>
2022-12-30 12:09:59 +0100

    [WOOCOMMERCE] Patch du fichier quantity input

--- storefront-child/woocommerce/global/quantity-input.php
+++ storefront-child/woocommerce/global/quantity-input.php
@@ -3,19 +3,34 @@
  * Product quantity inputs
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 4.0.0
+ * @version 7.2.1
  */
 
 defined( 'ABSPATH' ) || exit;
 /* translators: %s: Quantity. */
 $label = ! empty( $args['product_name'] ) ? sprintf( esc_html__( '%s quantity', 'woocommerce' ), wp_strip_all_tags( $args['product_name'] ) ) : esc_html__( 'Quantity', 'woocommerce' );
+
+// In some cases we wish to display the quantity but not allow for it to be changed.
+if ( $max_value && $min_value === $max_value ) {
+	$is_readonly = true;
+	$input_value = $min_value;
+} else {
+	$is_readonly = false;
+}
 ?>
 
 <div class="quantity">
-    <?php do_action( 'woocommerce_before_quantity_input_field' ); ?>
+    <?php
+	/**
+	 * Hook to output something before the quantity input field.
+	 *
+	 * @since 7.2.0
+	 */
+    do_action( 'woocommerce_before_quantity_input_field' ); ?>
     <label class="screen-reader-text" for="<?php echo esc_attr( $input_id ); ?>"><?php echo esc_attr( $label ); ?></label>
     <input
         type="number"
+        <?php echo $is_readonly ? 'readonly="readonly"' : ''; ?>
         id="<?php echo esc_attr( $input_id ); ?>"
         class="<?php echo esc_attr( join( ' ', (array) $classes ) ); ?>"
         step="<?php echo esc_attr( $step ); ?>"
@@ -29,5 +44,11 @@ $label = ! empty( $args['product_name'] ) ? sprintf( esc_html__( '%s quantity', 
         inputmode="<?php echo esc_attr( $inputmode ); ?>"
         autocomplete="<?php echo esc_attr( isset( $autocomplete ) ? $autocomplete : 'on' ); ?>"
     />
-    <?php do_action( 'woocommerce_after_quantity_input_field' ); ?>
+    <?php
+	/**
+	 * Hook to output something after quantity input field
+	 *
+	 * @since 3.6.0
+	 */
+    do_action( 'woocommerce_after_quantity_input_field' ); ?>
 </div>
